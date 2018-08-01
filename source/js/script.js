58eaa6681d27843c9f523abb07a2bf21f44d7dea
Sidjulik <sidjulik@gmail.com>
2018-08-01 19:56:26 +0300

    бургер готов

--- source/js/script.js
+++ source/js/script.js
@@ -1,17 +1,13 @@
-window.onload = function () {
-	var menuStyle = getComputedStyle (menu);
-	openMenu.onclick = function () {
-		if(menuStyle.display == "none") {
-			menu.classList.add ("active")
-		} else {
-      menu.classList.remove ("active")
-    }
-	}
-}
-window.onload = function () {
-  var toggle = document.querySelector('.page-header__toggle');
+var link = document.querySelector(".page-header__toggle");
+var popup = document.querySelector(".main-nav");
+var toggle = document.querySelector('.page-header__toggle');
 
-  toggle.addEventListener('click', function(e) {
-    this.classList.toggle('opened')
-  })
-}
+
+link.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  popup.classList.toggle("active");
+});
+
+toggle.addEventListener('click', function(e) {
+  this.classList.toggle('opened')
+});
